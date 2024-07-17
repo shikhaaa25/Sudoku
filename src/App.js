3b12f6da857c6ff15c58baf187c16e5710efaf9d
USER <shikhasharma0426@gmail.com>
2024-07-17 17:49:07 +0530

    code commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import SudokuGame from "./Components/SudokuGame";
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         {/* <nav>
+//           <ul>
+           
+//           </ul>
+//         </nav> */}
+//         <Routes>
+//           <Route path="/sudoku" element={<SudokuGame />} />
+//           <Route
+//             path="/"
+//             element={
+//               <div>
+//                 <h1>Welcome To Sudoku-Game</h1>
+           
+//                </div>
+//             }
+//           />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import SudokuGame from "./Components/SudokuGame";
+import 'animate.css';
+
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/sudoku" element={<SudokuGame />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/sudoku");
+  };
+
+  return (
+    <>
+   <h1 class="animate__heartBeat">Fun Time With Sudoku..!!</h1>
+    <div className="Home">
+      <h3 class="animate__heartBeat">Welcome To The Game</h3>
+      <button onClick={handleButtonClick}>Let's Play</button>
     </div>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/CommonComponents/CustomModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/modal.css';
+
+const Modal = ({ show, children, darkMode }) => {
+    if (!show) {
+        return null;
+    }
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal">
+                <div className={`modal-content ${darkMode ? "dark-mode" : ""}`}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
